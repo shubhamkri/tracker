e563c156fca5aa39c6164d70d5591ab50bfe9dd8
shubhamkri <shubhamshukla89sk@gmail.com>
2026-01-28 19:01:18 +0530

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,20 @@ terraform {
 
 provider "docker" {}
 
+# Docker image from DockerHub
+resource "docker_image" "expense_tracker" {
+  name = var.image_name
+}
+
+# Run container
 resource "docker_container" "app_container" {
   name  = "expense_tracker_app"
-  image = var.image_name
+  image = docker_image.expense_tracker.name
 
   ports {
     internal = 8501
     external = 8501
   }
+
+  restart = "always"
 }
